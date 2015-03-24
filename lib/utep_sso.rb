eea8008a6bfdbd20327dcd5a9a8c35a8b057e777
Alan Wernick <alan.wernik@outlook.com>
2015-03-23 22:46:34 -0600

    Fix weird spacing issues in file

--- lib/utep_sso.rb
+++ lib/utep_sso.rb
@@ -6,20 +6,20 @@ module UTEPSSO
 
   #{:get_user_by_ssiu_result=>{:user_name=>"awernick", :full_name=>"Alan Wernick, :email_address=>"awernick@miners.utep.edu", :authenticated=>true, :role_value=>"1080", :external_user=>false}, :@xmlns=>"http://tempuri.org/"}
   def self.authenticate(utep_cookie, utep_salt)
-		if authenticated?(utep_cookie, utep_salt)
-			@response.body[:get_user_by_ssiu_response][:get_user_by_ssiu_result]
-		end
-	end
+    if authenticated?(utep_cookie, utep_salt)
+      @response.body[:get_user_by_ssiu_response][:get_user_by_ssiu_result]
+    end
+  end
+  
+  def self.authenticated?(utep_cookie, utep_salt)
+    if utep_cookie && utep_salt
+	@response = @client.call(:get_user_by_ssiu, message: { sessionId: utep_cookie.to_s, salt: utep_salt.to_s })
+	@response.body[:get_user_by_ssiu_response][:get_user_by_ssiu_result][:authenticated]
+    end
+  end
 
-	def self.authenticated?(utep_cookie, utep_salt)
-		if utep_cookie && utep_salt
-			@response = @client.call(:get_user_by_ssiu, message: { sessionId: utep_cookie.to_s, salt: utep_salt.to_s })
-			@response.body[:get_user_by_ssiu_response][:get_user_by_ssiu_result][:authenticated]
-		end
-	end
-
-	def self.deauthenticate(utep_cookie, utep_salt)
-		if authenticated?(utep_cookie,utep_salt)
+  def self.deauthenticate(utep_cookie, utep_salt)
+    if authenticated?(utep_cookie,utep_salt)
       @client.call(:log_off, message: { sessionId: utep_cookie.to_s})
     end
   end
